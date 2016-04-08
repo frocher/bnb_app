fa1ba8b89e127d70ff40d98428c1b0a1b13ad311
Fred Rocher <frocher2005@gmail.com>
2016-04-08 16:54:10 +0200

    Optimize loading

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,25 @@
 (function(document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
 
   // Sets app default base URL
   app.baseUrl = '/';
 
-  /** Logged user informations **/
+  // Logged user informations
   app.user  = null;
 
-  /** Loaded pages */
+  // Loaded pages
   app.pages = null;
 
-  /** Current page */
+  // Current page
   app.page = null;
+
+  window.addEventListener('WebComponentsReady', function() {
+    var loadEl = document.getElementById('splash');
+    loadEl.addEventListener('transitionend', loadEl.remove);
+    loadEl.classList.remove('loading');
+  });
 
   app.isLogged = function() {
     return sessionStorage.getItem('accessToken') !== null;
