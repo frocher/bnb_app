3497951cdd198e170e25dee4e9065946be148cb4
Fred Rocher <frocher2005@gmail.com>
2018-04-22 22:07:23 +0200

    Add icon to push notif

--- push-handler.js
+++ push-handler.js
@@ -7,13 +7,16 @@ function handlePushEvent(event) {
   .then((data) => {
     const title = data.title;
     const options = data.options;
+    if (!options.icon) {
+      options.icon = '/es5-bundled/images/icon-512x512.png';
+    }
     return registration.showNotification(title, options);
   })
   .catch((err) => {
     const title = 'Message Received from Botnbot';
     const options = {
       body: event.data.text(),
-      icon: '/images/icon-128x128.png'
+      icon: '/es5-bundled/images/icon-512x512.png'
     };
     return registration.showNotification(title, options);
   });
